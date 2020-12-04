a99dfd7f6344f7bd703a3f191d94e106e8554207
HuHuHu <55592511+Wind-Gone@users.noreply.github.com>
2020-12-04 10:40:13 +0800

    first version

---
+++ README.md
@@ -0,0 +1,23 @@
+# gizmo-ball-vue
+
+本仓库是<https://github.com/Discreater/gizmoBall>的网页版。
+
+需要先安装[nodejs](https://nodejs.org/en/download/)以及[yarn](https://classic.yarnpkg.com/en/docs/install/#windows-stable)。
+
+## 安装依赖库
+
+```sh
+yarn install
+```
+
+### 本地开发模式
+
+```sh
+yarn run serve
+```
+
+### 打包构建
+
+```sh
+yarn run build
+```
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app" ref="app">
+    <Home></Home>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Home from "@/views/Home.vue";
+import TSViews from "@/views/TSViews";
+
+@Component({
+  components: {
+    Home
+  }
+})
+export default class HelloWorld extends Vue {
+  $refs!: {
+    app: HTMLElement;
+  };
+
+  mounted() {
+    // this.setAppSize();
+    TSViews.bindKey();
+  }
+
+  private setAppSize() {
+    this.$refs.app.style.height = `${window.innerHeight - 30}px`;
+    this.$refs.app.style.width = `${window.innerWidth}px`;
+  }
+}
+</script>
+
+<style>
+body {
+  user-select: none;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 1440;
+}
+
+#nav {
+  padding: 30px;
+}
+a {
+  font-weight: bold;
+  color: var(--color-text);
+}
+.router-link-exact-active {
+  color: #42b983;
+}
+</style>

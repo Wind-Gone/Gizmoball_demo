64fa595544c79bd693203a0867127d4ff69b7d83
HuHuHu <55592511+Wind-Gone@users.noreply.github.com>
2020-12-04 13:41:06 +0800

    将数据导入到本地csv文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,7 @@ import {MapItem} from "@/core/mapitems/map-item";
 import {itemMap} from "@/core/constants";
 import {MapItemJSON} from "@/core/controller/Map";
 import {isRotatable, isZoomable} from "@/core/common";
+import Papa from 'papaparse'
 
 @Component({
   components: {
@@ -58,44 +59,70 @@ export default class Home extends Vue {
     let content = Controller.getInstance().items;
     let length = content.length;
     const items: MapItem[] = [];
-    if (content instanceof Array) {
-      for (const jsonItem of content) {
-        const item = MapItemJSON.createFromJSON(jsonItem);
-        if (item !== null) {
-          const mapItem = new itemMap[item.name](
-              item.position.x,
-              item.position.y
-          );
-          // 检测是否旋转
-          if (isRotatable(mapItem)) {
-            if (item.rotation !== undefined) {
-              mapItem.rotation = item.rotation;
-            } else {
-              return null;
-            }
+    for (const jsonItem of content) {
+      const item = MapItemJSON.createFromJSON(jsonItem);
+      if (item !== null) {
+        const mapItem = new itemMap[item.name](
+            item.position.x,
+            item.position.y
+        );
+        // 检测是否旋转
+        if (isRotatable(mapItem)) {
+          if (item.rotation !== undefined) {
+            mapItem.rotation = item.rotation;
+          } else {
+            return;
           }
-          // 检测是否放缩
-          if (isZoomable(mapItem)) {
-            if (item.zoom !== undefined) {
-              mapItem.zoomTo(mapItem.position, item.zoom);
-            } else {
-              return null;
-            }
-          }
-          items.push(mapItem);
-        } else {
-          return null;
         }
-      }
-      for (let i = 0 ; i<length;i++){
-        console.log(items[i].name);
-        console.log(items[i].position.x);
-        console.log(items[i].position.y);
-        console.log(items[i].zoom);
-        console.log(items[i].rotation);
-
+        // 检测是否放缩
+        if (isZoomable(mapItem)) {
+          if (item.zoom !== undefined) {
+            mapItem.zoomTo(mapItem.position, item.zoom);
+          } else {
+            return;
+          }
+        }
+        items.push(mapItem);
+      } else {
+        return;
       }
     }
+    for (let i = 0; i < length; i++) {
+      console.log(items[i].name);
+      console.log(items[i].position.x);
+      console.log(items[i].position.y);
+      console.log(items[i].zoom);
+      console.log(items[i].rotation);
+    }
+    let heads = ['name', 'position_x', "position_y" , "zoom" , "rotation"]
+    let Data = [];
+    for(let i=0; i<length; i++) {
+      Data.push({
+        [heads[0]]: items[i].name,
+        [heads[1]]: items[i].position.x,
+        [heads[2]]: items[i].position.y,
+        [heads[3]]: items[i].zoom,
+        [heads[4]]: items[i].rotation,
+
+      })
+    }
+    console.log(Data)
+    const csv = Papa.unparse(Data);
+    console.log(csv)
+    //定义文件内容，类型必须为Blob 否则createObjectURL会报错
+    let content2 = new Blob([csv]);
+    //生成url对象
+    let urlObject = window.URL || window.webkitURL || window;
+    let url = urlObject.createObjectURL(content2);
+    //生成<a></a>DOM元素
+    let el = document.createElement("a");
+    //链接赋值
+    el.href = url;
+    el.download = "文件导出.cvs";
+    //必须点击否则不会下载
+    el.click();
+    //移除链接释放资源
+    urlObject.revokeObjectURL(url);
   }
 
   private load(): void {
